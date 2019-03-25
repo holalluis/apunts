e723b9272b5fbd05ac279f1e4bdb69148bb17a6f
Lluís Bosch <holalluis@gmail.com>
2019-03-25 17:14:37 +0100

    quick commit and push

--- C/factorial.c
+++ C/factorial.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int factorial(int n);
+long long int factorial(int n);
 
 int main(){
   int n;
   printf("Escriu un numero: ");
   scanf("%d",&n);
-  int f = factorial(n);
-  printf("El factorial de %d és %d\n",n,f);
+  long long int f = factorial(n);
+  printf("El factorial de %d és %lld\n",n,f);
   return 0;
 }
 
-int factorial(int n){
+long long int factorial(int n){
   if(n<=1){
     return 1;
   }else{
--- C/printf.c
+++ C/printf.c
@@ -18,4 +18,13 @@ int main(){
   double a = 5 + 5; //suma dos ints
   printf("%f\n", a);
 
+  /*3.3	Formatos de printf (avanzado)
+    Formato | Significado
+    --------+------------
+    %5d     | Entero decimal alineado a la izquierda, ocupando cinco espacios
+    %04u    | Entero sin signo ocupando cuatro espacios, y rellenando de ceros a la izquierda si hace falta
+    %.2lf   | Número real (doble precisión) con dos y sólo dos decimales
+    %5.3d   | Entero ocupando cinco espacios; aparecen tres cifras como mínimo (se rellena de ceros)
+*/
+
 return 0;}
